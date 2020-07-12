3dcad27de664b8ab889398d7b8eb2ac7ee8e187e
OskarPotocki <lordofcarbon@gmail.com>
2020-07-12 23:17:59 +0100

    Initial push

---
+++ 1.1/Source/VanillaBooksExpanded/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VanillaBooksExpanded
+{
+    public class Book : ThingWithComps
+    {
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            if (!respawningAfterLoad)
+            {
+                var comp = this.TryGetComp<CompBook>();
+                comp.InitializeBook();
+            }
+        }
+    }
+}
+
---
+++ 1.1/Source/VanillaBooksExpanded/CompProperties_Book.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaBooksExpanded
+{
+	public class CompProperties_Book : CompProperties
+	{
+		public RulePackDef nameMaker;
+
+		public RulePackDef descriptionMaker;
+
+		public QualityCategory minQualityForArtistic;
+
+		public bool mustBeFullGrave;
+
+		public bool canBeEnjoyedAsArt;
+
+		public string rulesStringName;
+
+		public string rulesStringDescription;
+
+		public CompProperties_Book()
+		{
+			compClass = typeof(CompBook);
+		}
+	}
+}
+
